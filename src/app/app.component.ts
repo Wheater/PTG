5aae25cb947efee9430d83fedb0bd740954e66a5
Eric Hurt <hurte@ptg.com>
2017-04-18 14:53:59 -0500

    Added solutions panel, hide PTG logo on scroll, styled footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component,
+         ElementRef,
+         HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app works!';
+  element: ElementRef;
+
+  constructor(private elementRef: ElementRef) {
+    this.element = elementRef;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event) {
+
+    var elementClassList = this.element.nativeElement.querySelector('.ptg-home-above-header').classList;
+
+    if(window.pageYOffset > 150) {
+      elementClassList.add('hidden');
+    } else {
+      elementClassList.remove('hidden');
+    }
+  }
 }
